082f40617b9adfa47203645943395a9004837c37
PawarAtishKumar <55620708+PawarAtishKumar@users.noreply.github.com>
2020-04-02 16:46:10 +0530

    Update SpringBootProject04EmployeeProjectApplication.java

--- SpringBoot-Project04-EmployeeProject/src/main/java/com/atish/SpringBootProject04EmployeeProjectApplication.java
+++ SpringBoot-Project04-EmployeeProject/src/main/java/com/atish/SpringBootProject04EmployeeProjectApplication.java
@@ -3,6 +3,9 @@ package com.atish;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+This Run Method will BootStrap the SpringBootApplication
+*/
 @SpringBootApplication
 public class SpringBootProject04EmployeeProjectApplication {
 
